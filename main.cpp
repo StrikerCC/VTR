34d41fe7f18880cb2b3c6181fae4e87b331e6267
Cheng Chen <chengc0611@gmail.com>
2022-07-20 18:58:01 +0800

    time counting done

--- main.cpp
+++ main.cpp
@@ -16,8 +16,7 @@ std::shared_ptr<open3d::geometry::RGBDImage> ReadRGBD(const std::string& file_pa
                                                       const std::string& file_path_rgb_img,
                                                       const std::string& file_path_depth_img);
 
-
-std::vector<std::shared_ptr<Eigen::Transform<double, 3, 0x1>>> ReadTfFromFile(const std::string& file_path_motion);
+std::vector<Eigen::Transform<double, 3, 0x1>> ReadTfFromFile(const std::string& file_path_motion);
 std::shared_ptr<open3d::geometry::VoxelGrid> DilateVoxelGrid(const open3d::geometry::VoxelGrid& voxel_grid_src, int voxel_size_dilating);
 std::shared_ptr<open3d::geometry::PointCloud> RemoveOverlappingVoxels(const open3d::geometry::PointCloud& pc_tgt, const open3d::geometry::PointCloud& pc_src, float dilating_voxel=20);
 int HasOverlapping(open3d::geometry::VoxelGrid& voxel_grid_tgt, const open3d::geometry::PointCloud& pc_src, open3d::geometry::VoxelGrid& voxel_grid_tgt_overlapping);
@@ -27,6 +26,7 @@ int main() {
 
     bool flag_output_debug = true;
     bool flag_vis_debug = true;
+    std::string indent = "      ";
 
     /// parameter setting
     int voxel_size = 20;
@@ -39,7 +39,7 @@ int main() {
     auto file_paths_camera_depth = std::make_shared<std::vector<std::string>> ();
     auto file_path_c_arm_motion = std::make_shared<std::string> ("");
 
-    std::cout << "Pwd " << std::filesystem::current_path() << std::endl;
+    std::cout << "$ Pwd: " << std::filesystem::current_path() << std::endl;
     for (int i_cam = 0; i_cam < num_camera; i_cam++) {
         file_paths_camera_parameter->push_back(data_folder + "/cam_para_" + std::to_string(i_cam) + ".json");
         file_paths_camera_rgb->push_back(data_folder + "/camera" + std::to_string(i_cam) + "_RGB.png");
@@ -73,19 +73,22 @@ int main() {
 
 //    auto voxel_grid_c_arm = open3d::geometry::VoxelGrid::CreateFromTriangleMesh(*pc_c_arm, voxel_size);
     auto voxel_grid_c_arm = open3d::geometry::VoxelGrid::CreateFromPointCloud(*pc_c_arm, voxel_size);
-//    auto c_arm_poses_motion_planning = ReadTfFromFile("");
-    auto c_arm_poses_motion_planning = std::vector<std::shared_ptr<Eigen::Transform<double, 3, 0x1>>>();
-    c_arm_poses_motion_planning.push_back(std::make_shared<Eigen::Transform<double, 3, 0x1>>(pose_c_arm_world));
-    c_arm_poses_motion_planning.push_back(std::make_shared<Eigen::Transform<double, 3, 0x1>>(Eigen::Matrix4d({{0, 0, 1, 100}, {1, 0, 0, -400}, {0, 1, 0, 850}, {0, 0, 0, 1}})));
-    c_arm_poses_motion_planning.push_back(std::make_shared<Eigen::Transform<double, 3, 0x1>>(Eigen::Matrix4d({{0, 0, 1, 300}, {1, 0, 0, -400}, {0, 1, 0, 150}, {0, 0, 0, 1}})));
+    auto c_arm_poses_motion_planning = std::vector<Eigen::Transform<double, 3, 0x1>>();
+    c_arm_poses_motion_planning.emplace_back(Eigen::Matrix4d({{0, 0, 1, 100}, {1, 0, 0, -400}, {0, 1, 0, 850}, {0, 0, 0, 1}}));
+    for (const auto &tf_motion_plan : ReadTfFromFile("")) {
+        c_arm_poses_motion_planning.push_back(tf_motion_plan * c_arm_poses_motion_planning.at(0));
+    }
+
+    //    c_arm_poses_motion_planning.push_back(std::make_shared<Eigen::Transform<double, 3, 0x1>>(pose_c_arm_world));
+//    c_arm_poses_motion_planning.push_back(std::make_shared<Eigen::Transform<double, 3, 0x1>>(Eigen::Matrix4d({{0, 0, 1, 300}, {1, 0, 0, -400}, {0, 1, 0, 150}, {0, 0, 0, 1}})));
+//    c_arm_poses_motion_planning.push_back(std::make_shared<Eigen::Transform<double, 3, 0x1>>(Eigen::Matrix4d({{0, 0, 1, 600}, {1, 0, 0, -400}, {0, 1, 0, 150}, {0, 0, 0, 1}})));
+
 
 
     /// visualization
     auto mesh_coord_frame = flag_vis_debug ? std::make_shared<open3d::geometry::TriangleMesh> ()->CreateCoordinateFrame(1000) : nullptr;
     if (bbox_c_arm_workspace && flag_vis_debug) bbox_c_arm_workspace->color_ = Eigen::Vector3d();
 
-//    auto mesh_c_arm_world = std::make_shared<open3d::geometry::PointCloud>(*pc_c_arm);
-//    mesh_c_arm_world->Transform(Eigen::Matrix4d({{0, 0, 1, -361}, {1, 0, 0, -400}, {0, 1, 0, 150}, {0, 0, 0, 1}}));     // transform c_arm to world frame
     if (flag_vis_debug) open3d::visualization::DrawGeometries({mesh_coord_frame, bbox_c_arm_workspace, pc_c_arm}, "c_arm from ply file");
 
     /// statistics
@@ -94,75 +97,72 @@ int main() {
 
     /// testing
     for (int i_iter = 0; i_iter < nLoop; i_iter++) {
-        /// read point cloud from camera
+        /// read raw data from camera
         auto time_start = std::chrono::high_resolution_clock::now();
         std::vector<std::shared_ptr<open3d::geometry::PointCloud>> pcs;     // point cloud array from triple camera
         auto pc_cam_combined = std::make_shared<open3d::geometry::PointCloud> ();   // point cloud merge from all cameras
 
         for (int i_cam = 0; i_cam < num_camera; i_cam++) {
             auto pc = open3d::geometry::PointCloud::CreateFromRGBDImage(*rgbds.at(i_cam), cam_models.at(i_cam)->intrinsic, cam_models.at(i_cam)->extrinsic);
-            if (flag_output_debug) std::cout << "point cloud remains " << pc->points_.size() << " / " << rgbds.at(i_cam)->color_.width_ * rgbds.at(i_cam)->color_.height_ << " pixels = " << 100.0 * (float) pc->points_.size() / (float) (rgbds.at(i_cam)->color_.width_ * rgbds.at(i_cam)->color_.height_) << " % " << std::endl;
+            if (flag_output_debug) std::cout << indent << "read point cloud from rgbd image remains " << pc->points_.size() << " / " << rgbds.at(i_cam)->color_.width_ * rgbds.at(i_cam)->color_.height_ << " pixels = " << 100.0 * (float) pc->points_.size() / (float) (rgbds.at(i_cam)->color_.width_ * rgbds.at(i_cam)->color_.height_) << " % " << std::endl;
             *pc_cam_combined += *pc;
         }
-        float dur_build_point_cloud = (float) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
-        cost_avg_build_pc += dur_build_point_cloud / (float) nLoop;
+        float dur_data_reading = (float) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
+        cost_avg_build_pc += dur_data_reading / (float) nLoop;
 
-        if (flag_output_debug) std::cout << "Point cloud built in " << dur_build_point_cloud << " milliseconds" << std::endl;
+        if (flag_output_debug) std::cout << "Data read in " << dur_data_reading << " milliseconds" << std::endl;
         if (flag_vis_debug) open3d::visualization::DrawGeometries({mesh_coord_frame, bbox_c_arm_workspace, pc_cam_combined});
 
-        /// voxel
+        /// preprocess data
         time_start = std::chrono::high_resolution_clock::now();
-        // build voxel from point cloud
-        // TODO: Point Removing
+        // remove c_arm from camera view point cloud
         {
             pc_cam_combined = pc_cam_combined->Crop(*bbox_c_arm_workspace);
-            // remove c_arm from camera view point cloud
             auto pc_c_arm_world = *pc_c_arm;
             pc_c_arm_world.Transform(pose_c_arm_world);
 
-            // vis debug
-            open3d::visualization::DrawGeometries({pc_cam_combined, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_world)}, "c_arm pose in real world");
+            // vis debug c_arm real pose in camera view
+            if (flag_vis_debug) open3d::visualization::DrawGeometries({pc_cam_combined, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_world)}, "c_arm pose in real world");
 
             auto pts_cam_with_c_arm = pc_cam_combined->points_.size();
             pc_cam_combined = RemoveOverlappingVoxels(*pc_cam_combined, pc_c_arm_world);
-            std::cout << "remove c_arm: removed " << pts_cam_with_c_arm - pc_cam_combined->points_.size() << " / " << pts_cam_with_c_arm << " = " << float (pts_cam_with_c_arm - pc_cam_combined->points_.size()) / (float) pts_cam_with_c_arm << " points from camera view" << std::endl;
 
-            // vis debug
-            open3d::visualization::DrawGeometries({pc_cam_combined, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_world)}, "c_arm removed in real world");
+            // debug c_arm removed from camera view
+            if (flag_output_debug) std::cout << indent << "remove c_arm from camera: removed " << pts_cam_with_c_arm - pc_cam_combined->points_.size() << " / " << pts_cam_with_c_arm << " = " << float (pts_cam_with_c_arm - pc_cam_combined->points_.size()) / (float) pts_cam_with_c_arm * 100 << "% points from camera view" << std::endl;
+            if (flag_vis_debug) open3d::visualization::DrawGeometries({pc_cam_combined, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_world)}, "c_arm removed in real world");
         }
         auto voxel_grid_room = open3d::geometry::VoxelGrid::CreateFromPointCloud(*pc_cam_combined, voxel_size);
 
-        // vis debug
-        open3d::visualization::DrawGeometries({voxel_grid_room}, "c_arm removed from camera view");
+        auto dur_data_processing = (float) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
+        cost_avg_build_voxel += dur_data_processing / (float) nLoop;
 
-        auto dur_build_voxel_grid = (float) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
-        cost_avg_build_voxel += dur_build_voxel_grid / (float) nLoop;
-
-//        if (flag_vis_debug) open3d::visualization::DrawGeometries({mesh_coord_frame, bbox_c_arm_workspace, voxel_grid_room});
-        if (flag_output_debug) std::cout << "Voxel built in " << dur_build_voxel_grid << " milliseconds" << std::endl;
+        if (flag_vis_debug) open3d::visualization::DrawGeometries({voxel_grid_room}, "c_arm removed from camera view");
+        if (flag_output_debug) std::cout << "Data precessed (get points in work space, remove c_arm, voxelization) in " << dur_data_processing << " milliseconds" << std::endl;
 
         /// collision detection
+        time_start = std::chrono::high_resolution_clock::now();
         // check overlapping
-        for (int i_c_arm_pose = 0; i_c_arm_pose < 10; i_c_arm_pose++) {     // rendering voxel as a_arm motion planned
+        for (int i_c_arm_pose = 0; i_c_arm_pose < c_arm_poses_motion_planning.size(); i_c_arm_pose++) {     // rendering voxel as a_arm motion planned
             auto pc_c_arm_moved_as_plan = *pc_c_arm;
-            pc_c_arm_moved_as_plan.Transform(c_arm_poses_motion_planning.at(i_c_arm_pose)->matrix());
+            pc_c_arm_moved_as_plan.Transform(c_arm_poses_motion_planning.at(i_c_arm_pose).matrix());
 
             // vis debug
             if (flag_vis_debug) open3d::visualization::DrawGeometries({voxel_grid_room, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_moved_as_plan)}, "c_arm at pose " + std::to_string(i_c_arm_pose));
 
             auto voxel_grid_tgt_overlapping = std::make_shared<open3d::geometry::VoxelGrid>();
             int num_of_overlapping = HasOverlapping(*voxel_grid_room, pc_c_arm_moved_as_plan, *voxel_grid_tgt_overlapping);
+
             // vis debug
-            std::cout << "pose " << i_c_arm_pose << " vtr # of collision: " << num_of_overlapping << std::endl;
-            open3d::visualization::DrawGeometries({voxel_grid_tgt_overlapping}, "c_arm pose " + std::to_string(i_c_arm_pose) + " overlapping");
-            open3d::visualization::DrawGeometries({voxel_grid_tgt_overlapping, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_moved_as_plan)}, "c_arm pose " + std::to_string(i_c_arm_pose) + " overlapping");
+            std::cout << indent << "pose " << i_c_arm_pose << ": " << c_arm_poses_motion_planning.at(i_c_arm_pose).matrix() << std::endl;
+            std::cout << indent << "pose " << i_c_arm_pose << " vtr # of collision: " << num_of_overlapping << std::endl;
+            if (flag_vis_debug) {
+                open3d::visualization::DrawGeometries({voxel_grid_tgt_overlapping}, "c_arm pose " + std::to_string(i_c_arm_pose) + " overlapping");
+                open3d::visualization::DrawGeometries({voxel_grid_tgt_overlapping, std::make_shared<open3d::geometry::PointCloud>(pc_c_arm_moved_as_plan)}, "c_arm pose " + std::to_string(i_c_arm_pose) + " overlapping");
+            }
         }
-
-//        if (flag_vis_debug) open3d::visualization::DrawGeometries({voxel_grid_room, voxel_grid_c_arm});
+        float dur_collision = (float) std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - time_start).count();
+        if (flag_output_debug) std::cout << "collision detected in " << dur_collision << " milliseconds" << std::endl;
     }
-
-//    std::cout << "collision detected in " << cost_avg_collision_det << " milliseconds" << std::endl;
-
     return 0;
 }
 
@@ -178,14 +178,10 @@ int HasOverlapping(open3d::geometry::VoxelGrid& voxel_grid_tgt, const open3d::ge
         for (int i_pt = 0; i_pt < pc_src.points_.size(); i_pt++) {
             if (included_src.at(i_pt)) voxel_grid_tgt_include->AddVoxel(open3d::geometry::Voxel(voxel_grid_tgt.GetVoxel(pc_src.points_.at(i_pt))));
         }
-        std::cout << "num of include " << voxel_grid_tgt_include->voxels_.size() << std::endl;
-        open3d::visualization::DrawGeometries({voxel_grid_tgt_include, std::make_shared<open3d::geometry::PointCloud>(pc_src)}, "points include");
-
     }
 
-    /// find overlapping cluster in target voxel grid
-    int id_collision_cluster = 0;
-    std::unordered_map<int, Eigen::Vector3d> color_Mapping;
+    int id_collision_cluster = 0;                               // cluster index
+    std::unordered_map<int, Eigen::Vector3d> color_Mapping;     // cluster color mapping
     color_Mapping[0] = Eigen::Vector3d(0.5, 0.5, 0);
     color_Mapping[1] = Eigen::Vector3d(0, 0.5, 0.5);
     color_Mapping[2] = Eigen::Vector3d(0.5, 0, 0.5);
@@ -194,20 +190,17 @@ int HasOverlapping(open3d::geometry::VoxelGrid& voxel_grid_tgt, const open3d::ge
     color_Mapping[5] = Eigen::Vector3d(0, 0, 1);
     color_Mapping[6] = Eigen::Vector3d(0.5, 0.5, 1);
     color_Mapping[7] = Eigen::Vector3d(1, 0.5, 0.5);
-//    color_Mapping[8] = Eigen::Vector3d(0.5, 1, 0.5);
+    color_Mapping[8] = Eigen::Vector3d(0.5, 1, 0.5);
 
-//    open3d::geometry::VoxelGrid voxel_grid_tgt_overlapping;
-    voxel_grid_tgt_overlapping.Clear();
+    voxel_grid_tgt_overlapping.Clear();     // overlapping voxel grid
     voxel_grid_tgt_overlapping.voxel_size_ = voxel_grid_tgt.voxel_size_;
     voxel_grid_tgt_overlapping.origin_ = voxel_grid_tgt.origin_;
 
     std::vector<Eigen::Vector3i> voxel_coords_overlapping;
 
+    /// find overlapping cluster in target voxel grid
     for (int i_pt_src = 0; i_pt_src < pc_src.points_.size(); i_pt_src++) {
         if (included_src.at(i_pt_src)) {    // this source point is included in target voxel grid
-//            return true;
-
-            // TODO: get cluster
             voxel_coords_overlapping.push_back(voxel_grid_tgt.GetVoxel(pc_src.points_.at(i_pt_src)));
 
             // deep-first search neighbors
@@ -256,8 +249,6 @@ int HasOverlapping(open3d::geometry::VoxelGrid& voxel_grid_tgt, const open3d::ge
 
 std::shared_ptr<open3d::geometry::VoxelGrid> DilateVoxelGrid(const open3d::geometry::VoxelGrid& voxel_grid_src, int voxel_size_dilating) {
     auto voxel_grid_dilating = std::make_shared<open3d::geometry::VoxelGrid>(voxel_grid_src);
-    open3d::visualization::DrawGeometries({voxel_grid_dilating});
-
     voxel_grid_dilating->Clear();
     voxel_grid_dilating->voxel_size_ = voxel_grid_src.voxel_size_;
     voxel_grid_dilating->origin_ = voxel_grid_src.origin_;
@@ -298,14 +289,12 @@ std::shared_ptr<open3d::geometry::PointCloud> RemoveOverlappingVoxels(const open
         for (int i_pt = 0; i_pt < pc_tgt.points_.size(); i_pt++) {
             if (tgt_pts_is_included.at(i_pt)) {
                 pc_tgt_include->points_.push_back(pc_tgt.points_.at(i_pt));
-                pc_tgt_include->colors_.push_back({0, 0.5, 0});
+                pc_tgt_include->colors_.emplace_back(0, 0.5, 0);
             } else {
                 pc_tgt_not_include->points_.push_back(pc_tgt.points_.at(i_pt));
-                pc_tgt_not_include->colors_.push_back({0, 0, 0.54});
+                pc_tgt_not_include->colors_.emplace_back(0, 0, 0.54);
             }
         }
-        std::cout << "num of include " << pc_tgt_include->points_.size() << std::endl;
-        open3d::visualization::DrawGeometries({std::make_shared<open3d::geometry::PointCloud>(pc_src), pc_tgt_include, pc_tgt_not_include}, "points include");
     }
 
     // remove overlapping points from target point cloud
@@ -321,10 +310,8 @@ std::shared_ptr<open3d::geometry::PointCloud> RemoveOverlappingVoxels(const open
     auto pc_tgt_not_include = std::make_shared<open3d::geometry::PointCloud>(pts_not_included);
     pc_tgt_not_include->colors_ = colors_not_included;
 
-
     // vis debug
-    open3d::visualization::DrawGeometries({pc_tgt_not_include, voxel_grid_src}, "c_arm pose in real world");
-
+//    open3d::visualization::DrawGeometries({pc_tgt_not_include, voxel_grid_src}, "c_arm pose in real world");
 
     return pc_tgt_not_include;
 }
@@ -342,16 +329,16 @@ std::shared_ptr<open3d::geometry::RGBDImage> ReadRGBD(const std::string& file_pa
     return img_rgbd;
 }
 
-std::vector<std::shared_ptr<Eigen::Transform<double, 3, 0x1>>> ReadTfFromFile(const std::string& file_path_motion) {
-    std::vector<std::shared_ptr<Eigen::Transform<double, 3, 0x1>>> tfs;
-    double step_t = 100, step_r = 18;
+std::vector<Eigen::Transform<double, 3, 0x1>> ReadTfFromFile(const std::string& file_path_motion) {
+    std::vector<Eigen::Transform<double, 3, 0x1>> tfs;
+    double step_t = 0, step_r = 18;
     for (int i_pose = 0; i_pose < 20; i_pose++) {
-        auto tf = std::make_shared<Eigen::Transform<double, 3, 0x1>>();
+        auto tf = Eigen::Transform<double, 3, 0x1>();
         Eigen::Matrix3d rotation = Eigen::Matrix3d(Eigen::AngleAxisd ((double) i_pose*step_r, Eigen::Vector3d::UnitZ())
                                                    * Eigen::AngleAxisd ((double) i_pose*step_r, Eigen::Vector3d::UnitY())
                                                    * Eigen::AngleAxisd ((double) i_pose*step_r, Eigen::Vector3d::UnitX()));
-        tf->translate(Eigen::Vector3d((double) i_pose*step_t, (double) i_pose*step_t, 0));
-        tf->rotate(Eigen::Matrix3d(
+        tf.translate(Eigen::Vector3d((double) i_pose*step_t, (double) i_pose*step_t, (double) i_pose*step_t));
+        tf.rotate(Eigen::Matrix3d(
                 Eigen::AngleAxisd ((double) i_pose*step_r, Eigen::Vector3d::UnitZ())
                 * Eigen::AngleAxisd ((double) i_pose*step_r, Eigen::Vector3d::UnitY())
                 * Eigen::AngleAxisd ((double) i_pose*step_r, Eigen::Vector3d::UnitX())));
